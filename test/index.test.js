88da48c5f00a24c14cd0124e39b0ad0ad70b2966
Umme Kulsum Tumpa <bsse1307@iit.du.ac.bd>
2025-01-09 07:13:33 +0600

    initial

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+//test/index.test.js
+const { add } = require('../index');
+
+test('adds two numbers correctly', () => {
+  expect(add(1, 2)).toBe(3);
+  expect(add(1, "asdf")).toBe("Invalid Input");
+});
